65e383937efee2a9b36517f7da8c74827470cbba
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 11:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
+  
   for(let i = 0; i < musicians.length; i++) {
     newArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  
   return newArray;
 }
 
 function johnLennonFacts(facts) {
   var i = 0;
-  
+
   while(i < facts.length) {
     facts[i] = `${facts[i]}!!!`;
     i += 1;
   }
 
   return facts;
+}
+
+function iLoveTheBeatles(number) {
+  do {
+    
+  } while ();
 }
